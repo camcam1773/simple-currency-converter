6fa89f4208ee727aaa9f05fe5e4b2891c395cb5f
Mel C <camcam1773@users.noreply.github.com>
2021-01-11 21:13:25 +0100

    Initial commit

--- exchange_rate.py
+++ exchange_rate.py
@@ -18,22 +18,11 @@ class Currency:
         return amount * self.rates.get(to)
 
 
-# def get_one_rate(a: str, b: str) -> dict:
-#     local_url= Currency.base_url + '/latest?base_url=' + a + '&symbols=' + b
-#     try:
-#         with req.urlopen(local_url, timeout=10) as f:
-#             json_data = loads(f.read())
-#     except Exception as e:
-#         print(e, local_url)
-#     return json_data
-
-
 def get_all_for_base(a: str) -> dict:
-    with req.urlopen(Currency.base_url + '/latest?base=' + a, timeout=10) as f:
-        json_data: dict = loads(f.read())
+    local_url = Currency.base_url + '/latest?base=' + a
+    try:
+        with req.urlopen(local_url, timeout=10) as f:
+            json_data: dict = loads(f.read())
+    except Exception as e:
+        print(e, local_url)
     return json_data
-
-
-if __name__ == '__main__':
-    am1 = get_all_for_base('USD')
-    b1 = Currency(am1)
