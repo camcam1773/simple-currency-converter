e99767f2ed8f9d62208c6a9013bff9088a8ee344
Mel C <camcam1773@users.noreply.github.com>
2021-02-28 13:45:05 +0100

    Applied black formatter

--- exchange_rate.py
+++ exchange_rate.py
@@ -5,21 +5,53 @@ from json import loads
 class Currency:
     base_url: str = "https://api.exchangeratesapi.io"
     timeout: int = 10
-    codes = ['AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP', 'HKD', 'HRK', 'HUF', 'IDR', 'ILS',
-             'INR', 'ISK', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB',
-             'TRY', 'USD', 'ZAR']
+    codes = [
+        "AUD",
+        "BGN",
+        "BRL",
+        "CAD",
+        "CHF",
+        "CNY",
+        "CZK",
+        "DKK",
+        "EUR",
+        "GBP",
+        "HKD",
+        "HRK",
+        "HUF",
+        "IDR",
+        "ILS",
+        "INR",
+        "ISK",
+        "JPY",
+        "KRW",
+        "MXN",
+        "MYR",
+        "NOK",
+        "NZD",
+        "PHP",
+        "PLN",
+        "RON",
+        "RUB",
+        "SEK",
+        "SGD",
+        "THB",
+        "TRY",
+        "USD",
+        "ZAR",
+    ]
 
     def __init__(self, my_dict: dict):
-        self.base_currency = my_dict['base']
-        self.date = my_dict['date']
-        self.rates = my_dict['rates']
+        self.base_currency = my_dict["base"]
+        self.date = my_dict["date"]
+        self.rates = my_dict["rates"]
 
     def convert_amount_to(self, amount, to: str):
         return amount * self.rates.get(to)
 
 
 def get_all_for_base(a: str) -> dict:
-    local_url = Currency.base_url + '/latest?base=' + a
+    local_url = Currency.base_url + "/latest?base=" + a
     with req.urlopen(local_url, timeout=10) as f:
         json_data: dict = loads(f.read())
     return json_data
--- main.py
+++ main.py
@@ -18,17 +18,17 @@ def button_ok_action():
     from_currency = dropdown_text1.get()
     to_currency = dropdown_text2.get()
     amount = field1.get()
-    if amount == '':
+    if amount == "":
         return
     try:
         amount = float(amount)
     except ValueError:
-        result.set('NaN')
+        result.set("NaN")
         return
     try:
-        rate: float = er.get_all_for_base(from_currency)['rates'].get(to_currency, 1)
+        rate: float = er.get_all_for_base(from_currency)["rates"].get(to_currency, 1)
     except Exception as e:
-        result.set('Con. Failed!')
+        result.set("Con. Failed!")
         print(e)
         return
     converted = amount * rate
@@ -40,23 +40,23 @@ def key_enter_action(event):
 
 
 root = tk.Tk()
-root.title('CC')
-root.option_add('*Font', '72')
+root.title("CC")
+root.option_add("*Font", "72")
 
 root.resizable(False, False)
-label1 = tk.Label(root, text='Currency Converter')
+label1 = tk.Label(root, text="Currency Converter")
 field1 = tk.Entry(root, width=10)
-button_ok = tk.Button(root, text='Convert', command=button_ok_action)
-button_exit = tk.Button(root, text='Close', command=close)
+button_ok = tk.Button(root, text="Convert", command=button_ok_action)
+button_exit = tk.Button(root, text="Close", command=close)
 dropdown_text1 = tk.StringVar(root)
 dropdown_text2 = tk.StringVar(root)
 result = tk.StringVar(root)
 label_result = tk.Label(root, textvariable=result)
 
 codes = er.Currency.codes
-dropdown_text1.set('USD')
+dropdown_text1.set("USD")
 dropdown1 = tk.OptionMenu(root, dropdown_text1, *codes)
-dropdown_text2.set('EUR')
+dropdown_text2.set("EUR")
 dropdown2 = tk.OptionMenu(root, dropdown_text2, *codes)
 
 label1.grid(row=0, padx=5)
@@ -66,7 +66,7 @@ field1.grid(row=2, column=0, padx=1, pady=1)
 label_result.grid(row=2, column=1, padx=1, pady=1)
 button_ok.grid(row=3, column=0)
 button_exit.grid(row=3, column=1)
-root.bind('<Return>', key_enter_action)
-root.geometry('250x130')
+root.bind("<Return>", key_enter_action)
+root.geometry("250x130")
 
 root.mainloop()
